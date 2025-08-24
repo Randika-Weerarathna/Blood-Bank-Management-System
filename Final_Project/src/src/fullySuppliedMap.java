7cdbe515e559137f2929a8e5a882fde825b3f0d5
Randika-Weerarathna <randichamindi17@gmail.com>
2025-08-24 21:48:25 +0530

    Add files via upload

---
+++ Final_Project/src/src/fullySuppliedMap.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+class fullySuppliedMap {
+
+    static boolean getOrDefault(String requestID, boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
